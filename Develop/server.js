366b3e1bdda36d5be3e612ed6ac75809a6040e2a
Anitha Venkatesan <anithamca68@gmail.com>
2020-07-25 23:05:49 -0500

    created html routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const db = require("./model/userModel.js");
@@ -15,8 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
-  
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true});
+//Creating html routes
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+  });
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/stats.html"));
+  });
+
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
