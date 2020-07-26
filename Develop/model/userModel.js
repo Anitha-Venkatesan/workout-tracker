5f6b9490becc6065efb4a209b95b10b5febefa26
Anitha Venkatesan <anithamca68@gmail.com>
2020-07-25 22:13:42 -0500

    added model folder

---
+++ Develop/model/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  day: { 
+      type: Date, 
+      required: true, 
+      default: Date.now 
+  },
+  type: {
+    type: String,
+    trim: true,
+    required: true
+  },
+
+   name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+
+  duration: {
+    type: Number,
+    trim: true,
+  },
+
+  weight: {
+    type: Number,
+    trim: true
+  },
+
+  distance: {
+    type: Number,
+    trim: true
+    
+  },
+  distance: {
+    type: Number,
+    trim: true
+    
+  },
+  reps: {
+    type: Number,
+    trim: true
+    
+  },
+  sets: {
+    type: Number,
+    trim: true,
+  } 
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
