624845a28ca13d407af2796153f7b98e8d3643fc
Anitha Venkatesan <anithamca68@gmail.com>
2020-07-27 17:19:04 -0500

    Created routes for api/workouts/range

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,6 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const User = require("./model/userModel");
-//const workout = require("./public/workout");
 
 const app = express();
 
@@ -18,13 +17,25 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true});
 //Creating html routes
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+  });
   app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/exercise.html"));
   });
   app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/stats.html"));
   });
-  //creating api routes
+
+  app.get("/api/workouts/range", (req, res) => {
+    User.find({})
+        .then(dbUser => {
+        res.json(dbUser);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+  });
 
   app.get("/api/workouts", (req, res) => {
     User.find({})
@@ -35,7 +46,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { u
         res.json(err);
     });
   });
-
   app.post("/api/workouts", ({ body }, res) => {
     const user = new User(body);
         User.create(user)
@@ -46,14 +56,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { u
             res.json(err);
           });
   }); 
-  app.put("/api/workouts/:id",function(req,res) {
+
+  app.put("/api/workouts/:id",(req,res,next) => {
       console.log(req.body);
       console.log(req.params.id);
-      User.findByIdAndUpdate(req.params.id,{ $push: { exercise: req.body } }, function(err, post) {
+      User.findByIdAndUpdate(req.params.id,{ $push: { exercises: req.body } }, function(err, post) {
         if (err) return next(err);
         res.json(post);
        });
-  });    
+  });   
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
