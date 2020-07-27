8db2274fe04432eb861bbddc4fc543dc4d6b16c4
Anitha Venkatesan <anithamca68@gmail.com>
2020-07-27 17:22:26 -0500

    Added total duration for workouts, it was showing undefined

--- Develop/public/workout.js
+++ Develop/public/workout.js
@@ -28,6 +28,8 @@ function tallyExercises(exercises) {
     } else if (curr.type === "cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
     }
+    //Added total duration for exercise array
+    acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
     return acc;
   }, {});
   return tallied;
